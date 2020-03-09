a1e4e996c41ff2be40fc8bf5e3ff01652092a5ef
Parker <mparkermarriott@gmail.com>
2020-03-09 15:19:36 -0700

    Indexer Code for "In-between Motors" and Intake Motor

--- FRC2020/src/main/java/frc/robot/subsystems/IndexerAndRoller.java
+++ FRC2020/src/main/java/frc/robot/subsystems/IndexerAndRoller.java
@@ -16,56 +16,73 @@ public class IndexerAndRoller extends Subsystem {
         return mInstance;
     }
 
+    // Indexer Middle
     private final DigitalInput mIRSensors[];
-    private final boolean mPrevious[];
-    private final boolean mMotors[];
-    private boolean mIsEmpty;
-    private final boolean mDifference;
+    private final TalonSRX mIndexerMotors[];
+    private final TalonSRX mIndexerMotor1;
+    private final TalonSRX mIndexerMotor2;
+    private final TalonSRX mIndexerMotor3;
+    private final TalonSRX mIndexerMotor4;
+
+    // Roller
     private final TalonSRX mRollerMotor;
 
     private IndexerAndRoller() {
+        // Indexer Middle
         mIRSensors = new DigitalInput[5];
-        mPrevious = new boolean[mIRSensors.length + 1];
-        mMotors = new boolean[mPrevious.length];
-        mIsEmpty = true;
-        mDifference = false;
-        mRollerMotor = new TalonSRX(Constants.kRollerMotorId);
-
+        mIndexerMotor1 = new TalonSRX(Constants.kIndexerMotorId1);
+        mIndexerMotor2 = new TalonSRX(Constants.kIndexerMotorId2);
+        mIndexerMotor3 = new TalonSRX(Constants.kIndexerMotorId3);
+        mIndexerMotor4 = new TalonSRX(Constants.kIndexerMotorId4);
+        mIndexerMotors = new TalonSRX[4];
+        mIndexerMotors[0] = mIndexerMotor1;
+        mIndexerMotors[1] = mIndexerMotor2;
+        mIndexerMotors[2] = mIndexerMotor3;
+        mIndexerMotors[3] = mIndexerMotor4;
         for (int i = 0; i < mIRSensors.length; i++) {
             mIRSensors[i] = new DigitalInput(i);
         }
 
-        for (int i = 0; i < mPrevious.length; i++) {
-            mPrevious[i] = false;
-            mMotors[i] = false;
-        }
+        // Roller
+        mRollerMotor = new TalonSRX(Constants.kRollerMotorId);
     }
 
     public synchronized void setIndexer(boolean rollerDownAndOn) {
-        if (rollerDownAndOn) {
+        // Flywheel Insert
+
+
+        // Indexer Middle
+        for (int i = 1; i < mIRSensors.length; i ++) {
+            // When there is a ball below a unfilled spot, move it up 1 spot
+            if (mIRSensors[i].get() == false && mIRSensors[i-1].get() == true) {
+                mIndexerMotors[i].set(TalonSRXControlMode.PercentOutput, 20);
+                mIndexerMotors[i-1].set(TalonSRXControlMode.PercentOutput, 20);
+            }
+            // Dont turn on motors when spot above has ball, or there is no ball in spot
+            else {
+                mIndexerMotors[i].set(TalonSRXControlMode.PercentOutput, 0);
+            }
+        }
+
+        // Roller
+
+        // When 'Y' is pressed and there is no ball in the bottom slot, turn the roller motor on to 20%
+        if (rollerDownAndOn && mIRSensors[5].get() == true) {
             mRollerMotor.set(TalonSRXControlMode.PercentOutput, 20);
         }
-
-        if (rollerDownAndOn && mIRSensors[0].get() == false) {
-            mMotors[0] = true;
-        } else {
-            mMotors[0] = false;
+        // When 'Y' is pressed but there is a ball in the bottom slot, dont turn the roller motor on
+        else if (rollerDownAndOn && mIRSensors[5].get() == false) {
+            mRollerMotor.set(TalonSRXControlMode.PercentOutput, 0);
         }
+    }
 
-        for (int i = 0; i < mIRSensors.length; i ++) {
-            if (mIRSensors[i].get() == false && mIsEmpty == true) {
-                mMotors[i + 1] = false;
-            } else {
-                mMotors[i + 1] = true;
-                mIsEmpty = true;
-            }
+    public boolean isIndexerFinished() {
+        for (DigitalInput digitalInput : mIRSensors) {
+            if (digitalInput.get() == false) {
+                return false;
+            }   
         }
-
-        if (mDifference) {
-            for (int i = 0; i < mMotors.length; i ++) {
-                System.out.println(i + ":" + mMotors[i] + ",");
-            }
-        }
+        return true;
     }
 
     @Override
