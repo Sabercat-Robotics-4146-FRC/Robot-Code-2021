4b02b7380228af0baaf85b671d9dfb0de50f478a
lesser618 <lesser618@gmail.com>
2021-02-17 19:25:22 -0700

    updated gradle and spotless

--- FRC2021/src/main/java/frc/robot/Robot.java
+++ FRC2021/src/main/java/frc/robot/Robot.java
@@ -12,51 +12,53 @@ import frc.robot.controller.XboxController;
 import frc.robot.controller.XboxController.Axis;
 import frc.robot.controller.XboxController.Button;
 import frc.robot.controller.XboxController.Side;
-import frc.robot.Constants;
 import frc.robot.loops.Looper;
 import frc.robot.subsystems.Drive;
 
 public class Robot extends TimedRobot {
-	Looper mEnabledLooper = new Looper();
-	Looper mDisabledLooper = new Looper();
+  Looper mEnabledLooper = new Looper();
+  Looper mDisabledLooper = new Looper();
 
-	private final SubsystemManager mSubsystemManager = SubsystemManager.getInstance();
+  private final SubsystemManager mSubsystemManager = SubsystemManager.getInstance();
 
-	private Drive mDrive;
-	
-	private XboxController mDriver1XboxController;
+  private Drive mDrive;
 
-	@Override
-	public void robotInit() {
-		mDrive = Drive.getInstance();
-		mSubsystemManager.setSubsystems(mDrive);
+  private XboxController mDriver1XboxController;
 
-		mDriver1XboxController = new XboxController(Constants.kDriver1USBPort);
+  @Override
+  public void robotInit() {
+    mDrive = Drive.getInstance();
+    mSubsystemManager.setSubsystems(mDrive);
 
-		mSubsystemManager.registerEnabledLoops(mEnabledLooper);
-		mSubsystemManager.registerDisabledLoops(mDisabledLooper);
-	}
+    mDriver1XboxController = new XboxController(Constants.kDriver1USBPort);
 
-	@Override
-	public void autonomousInit() {
-		mDisabledLooper.stop();
-		mEnabledLooper.start();
-	}
+    mSubsystemManager.registerEnabledLoops(mEnabledLooper);
+    mSubsystemManager.registerDisabledLoops(mDisabledLooper);
+  }
 
-	@Override
-	public void disabledInit() {
-		mEnabledLooper.stop();
-		mDisabledLooper.start();
-	}
+  @Override
+  public void autonomousInit() {
+    mDisabledLooper.stop();
+    mEnabledLooper.start();
+  }
 
-	@Override
-	public void teleopInit() {
-		mDisabledLooper.stop();
-		mEnabledLooper.start();
-	}
+  @Override
+  public void disabledInit() {
+    mEnabledLooper.stop();
+    mDisabledLooper.start();
+  }
 
-	@Override
-	public void teleopPeriodic() {
-		mDrive.setCheesyishDrive(mDriver1XboxController.getJoystick(Side.LEFT,Axis.Y), -mDriver1XboxController.getJoystick(Side.RIGHT, Axis.X), mDriver1XboxController.getButton(Button.A));
-	}
+  @Override
+  public void teleopInit() {
+    mDisabledLooper.stop();
+    mEnabledLooper.start();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    mDrive.setCheesyishDrive(
+        mDriver1XboxController.getJoystick(Side.LEFT, Axis.Y),
+        -mDriver1XboxController.getJoystick(Side.RIGHT, Axis.X),
+        mDriver1XboxController.getButton(Button.A));
+  }
 }
--- FRC2021/src/main/java/frc/robot/subsystems/Flywheel.java
+++ FRC2021/src/main/java/frc/robot/subsystems/Flywheel.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.drivers.TalonSRXFactory;
 import frc.lib.drivers.TalonSRXUtil;
@@ -12,114 +11,123 @@ import frc.robot.loops.ILooper;
 import frc.robot.loops.Loop;
 
 public class Flywheel extends Subsystem {
-    private static Flywheel mInstance;
+  private static Flywheel mInstance;
 
-    public static Flywheel getInstance() {
-        if (mInstance == null) {
-            mInstance = new Flywheel();
-        }
-
-        return mInstance;
+  public static Flywheel getInstance() {
+    if (mInstance == null) {
+      mInstance = new Flywheel();
     }
 
-    private final TalonSRX mMaster, mSlave;
+    return mInstance;
+  }
 
-    public class PeriodicIO {
-        // inputs
-        double velocity_ticks_per_100_ms = 0.0;
-        
-        // outputs
-        double demand = 0.0;
-    }
+  private final TalonSRX mMaster, mSlave;
 
-    private PeriodicIO mPeriodicIO = new PeriodicIO();
+  public class PeriodicIO {
+    // inputs
+    double velocity_ticks_per_100_ms = 0.0;
 
-    private Flywheel() {
-        mMaster = TalonSRXFactory.createDefaultTalon(Constants.kFlywheelMasterId);
+    // outputs
+    double demand = 0.0;
+  }
 
-        // initialize encoder
-        TalonSRXUtil.checkError(mMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,
-                Constants.kLongCANTimeoutMs), "Flywheel: Could not detect encoder: ");
+  private PeriodicIO mPeriodicIO = new PeriodicIO();
 
-        // set gains
-        TalonSRXUtil.checkError(mMaster.config_kP(0, Constants.kFlywheelKp, Constants.kLongCANTimeoutMs),
-            "Flywheel: could not set kP: ");
-        TalonSRXUtil.checkError(mMaster.config_kI(0, Constants.kFlywheelKi, Constants.kLongCANTimeoutMs),
-            "Flywheel: could not set kI: ");
-        TalonSRXUtil.checkError(mMaster.config_kD(0, Constants.kFlywheelKd, Constants.kLongCANTimeoutMs),
-            "Flywheel: could not set kD: ");
-        TalonSRXUtil.checkError(mMaster.config_kF(0, Constants.kFlywheelKf, Constants.kLongCANTimeoutMs),
-            "Flywheel: Could not set kF: ");
+  private Flywheel() {
+    mMaster = TalonSRXFactory.createDefaultTalon(Constants.kFlywheelMasterId);
 
-        mSlave = TalonSRXFactory.createPermanentSlaveTalon(Constants.kFlywheelSlaveId, Constants.kFlywheelMasterId);
-    }
+    // initialize encoder
+    TalonSRXUtil.checkError(
+        mMaster.configSelectedFeedbackSensor(
+            FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.kLongCANTimeoutMs),
+        "Flywheel: Could not detect encoder: ");
 
-    @Override
-    public void registerEnabledLoops(ILooper mEnabledLooper) {
-        mEnabledLooper.register(new Loop() {
-            @Override
-            public void onStart(double timestamp) {}
-        
-            @Override
-            public void onLoop(double timestamp) {}
+    // set gains
+    TalonSRXUtil.checkError(
+        mMaster.config_kP(0, Constants.kFlywheelKp, Constants.kLongCANTimeoutMs),
+        "Flywheel: could not set kP: ");
+    TalonSRXUtil.checkError(
+        mMaster.config_kI(0, Constants.kFlywheelKi, Constants.kLongCANTimeoutMs),
+        "Flywheel: could not set kI: ");
+    TalonSRXUtil.checkError(
+        mMaster.config_kD(0, Constants.kFlywheelKd, Constants.kLongCANTimeoutMs),
+        "Flywheel: could not set kD: ");
+    TalonSRXUtil.checkError(
+        mMaster.config_kF(0, Constants.kFlywheelKf, Constants.kLongCANTimeoutMs),
+        "Flywheel: Could not set kF: ");
 
-            @Override
-            public void onStop(double timestamp) {
-                stop();
-            }
+    mSlave =
+        TalonSRXFactory.createPermanentSlaveTalon(
+            Constants.kFlywheelSlaveId, Constants.kFlywheelMasterId);
+  }
+
+  @Override
+  public void registerEnabledLoops(ILooper mEnabledLooper) {
+    mEnabledLooper.register(
+        new Loop() {
+          @Override
+          public void onStart(double timestamp) {}
+
+          @Override
+          public void onLoop(double timestamp) {}
+
+          @Override
+          public void onStop(double timestamp) {
+            stop();
+          }
         });
-    }
+  }
 
-    @Override
-    public void readPeriodicInputs() {
-        mPeriodicIO.velocity_ticks_per_100_ms = mMaster.getSelectedSensorPosition(0);
-    }
+  @Override
+  public void readPeriodicInputs() {
+    mPeriodicIO.velocity_ticks_per_100_ms = mMaster.getSelectedSensorPosition(0);
+  }
 
-    @Override
-    public void writePeriodicOutputs() {
-        mMaster.set(ControlMode.Velocity, mPeriodicIO.demand);
-    }
+  @Override
+  public void writePeriodicOutputs() {
+    mMaster.set(ControlMode.Velocity, mPeriodicIO.demand);
+  }
 
-    @Override
-    public void stop() {
-        mMaster.set(ControlMode.PercentOutput, 0.0);
-    }
+  @Override
+  public void stop() {
+    mMaster.set(ControlMode.PercentOutput, 0.0);
+  }
 
-    @Override
-    public boolean checkSystem() {
-        return false;
-    }
+  @Override
+  public boolean checkSystem() {
+    return false;
+  }
 
-    public synchronized void setRPM(double rpm) {
-        mPeriodicIO.demand = rpmToNativeUnits(rpm);
-    }
+  public synchronized void setRPM(double rpm) {
+    mPeriodicIO.demand = rpmToNativeUnits(rpm);
+  }
 
-    public synchronized double getRPM() {
-        return nativeUnitsToRPM(getVelocityNativeUnits());
-    }
+  public synchronized double getRPM() {
+    return nativeUnitsToRPM(getVelocityNativeUnits());
+  }
 
-    public synchronized double getVelocityNativeUnits() {
-        return mPeriodicIO.velocity_ticks_per_100_ms;
-    }
+  public synchronized double getVelocityNativeUnits() {
+    return mPeriodicIO.velocity_ticks_per_100_ms;
+  }
 
-    /**
-     * @param ticks per 100 ms
-     * @return rpm
-     */
-    public double nativeUnitsToRPM(double ticks_per_100_ms) {
-        return ticks_per_100_ms * 10.0 * 60.0 / Constants.kFlywheelTicksPerRevolution;
-    }
+  /**
+   * @param ticks per 100 ms
+   * @return rpm
+   */
+  public double nativeUnitsToRPM(double ticks_per_100_ms) {
+    return ticks_per_100_ms * 10.0 * 60.0 / Constants.kFlywheelTicksPerRevolution;
+  }
 
-    /**
-     * @param rpm
-     * @return ticks per 100 ms
-     */
-    public double rpmToNativeUnits(double rpm) {
-        return rpm / 60.0 / 10.0 * Constants.kFlywheelTicksPerRevolution;
-    }
+  /**
+   * @param rpm
+   * @return ticks per 100 ms
+   */
+  public double rpmToNativeUnits(double rpm) {
+    return rpm / 60.0 / 10.0 * Constants.kFlywheelTicksPerRevolution;
+  }
 
-    @Override
-    public void outputTelemetry() {
-        SmartDashboard.putNumber("Flywheel RPM", getRPM());
-    }
+  @Override
+  public void outputTelemetry() {
+    SmartDashboard.putNumber("Flywheel RPM", getRPM());
+  }
 }
