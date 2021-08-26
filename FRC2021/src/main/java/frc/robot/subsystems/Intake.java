e6ba5622c7246a7a6ca0a1839e38ea4508d1801c
Parker <mparkermarriott@gmail.com>
2021-08-26 16:19:16 -0700

    Code Cleanup Part 1 - Intake
    Cleaned up and added comments to Intake to improve readability

--- FRC2021/src/main/java/frc/robot/subsystems/Intake.java
+++ FRC2021/src/main/java/frc/robot/subsystems/Intake.java
@@ -27,50 +27,58 @@ public class Intake extends Subsystem {
   private final CANSparkMax mSparkMaxID11;
 
   private Intake() {
+    // Initialize Infrared Sensors
     mIRSensor1 = new DigitalInput(2);
     mIRSensor2 = new DigitalInput(3);
     mIRSensor3 = new DigitalInput(4);
 
+    // Initialize Motors
     mSparkMaxID14 = new CANSparkMax(14, MotorType.kBrushless);
     mSparkMaxID13 = new CANSparkMax(13, MotorType.kBrushless);
     mSparkMaxID12 = new CANSparkMax(12, MotorType.kBrushless);
     mSparkMaxID11 = new CANSparkMax(11, MotorType.kBrushless);
   }
 
-  public synchronized void setIndexer(
-      boolean shootBall, boolean startIntake, boolean reverseToggle) {
+  public synchronized void setIndexer(boolean shootBall, boolean startIntake, boolean reverseToggle) {
+
     if (reverseToggle == true) {
+      // "Panic Button" - Ejects balls by putting motors in reverse motion
       mSparkMaxID14.set(.5);
       mSparkMaxID13.set(.5);
       mSparkMaxID12.set(.5);
       mSparkMaxID11.set(.5);
     } else {
+      //Called if Reverse is not active
       if (startIntake == false) {
+        // Motors are hard stopped unless startIntake is called
         mSparkMaxID14.stopMotor();
         mSparkMaxID13.stopMotor();
         mSparkMaxID12.stopMotor();
         mSparkMaxID11.stopMotor();
       } else {
+        //Only called if Reverse is not active and startIntake is called
+        //Code to check ball position in indexer, and consequently turn off and on belts
         if (mIRSensor3.get() == true) {
+          //If top sensor does not see a ball: All belts turn on
           mSparkMaxID12.set(-.5);
           mSparkMaxID13.set(-.5);
           mSparkMaxID14.set(-.5);
           // mSparkMaxID11.set(-.2);
         }
-
         if (mIRSensor3.get() == false && mIRSensor2.get() == true) {
+          //If top sensor sees a ball but middle sensor does not: Only bottom and middle belts turn on
           mSparkMaxID12.set(0);
           mSparkMaxID13.set(-.5);
           mSparkMaxID14.set(-.5);
         }
-
         if (mIRSensor3.get() == false && mIRSensor2.get() == false && mIRSensor1.get() == true) {
+          //If top and middle sensor have ball and bottom does not: Only bottom belt turns on
           mSparkMaxID12.set(0);
           mSparkMaxID13.set(0);
           mSparkMaxID14.set(-.5);
         }
-
         if (mIRSensor3.get() == false && mIRSensor2.get() == false && mIRSensor1.get() == false) {
+          //If all sensors have a ball: Belts are off
           mSparkMaxID12.set(0);
           mSparkMaxID13.set(0);
           mSparkMaxID14.set(0);
@@ -78,11 +86,15 @@ public class Intake extends Subsystem {
       }
 
       if (shootBall == true) {
+        //To be used when indexer is full
+        //All belts turn on to shoot the ball - Belts ramp in speed to make sure balls dont get stuck
         mSparkMaxID14.set(-.2);
         mSparkMaxID13.set(-.35);
         mSparkMaxID12.set(-.55);
         mSparkMaxID11.set(.6);
       } else {
+        //If not shooting balls: Top Indexer (Feed Motor) is off
+        //Basically choke point so balls aren't constantly being shot
         mSparkMaxID11.stopMotor();
       }
     }
